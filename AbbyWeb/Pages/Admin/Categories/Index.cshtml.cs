dbe757a0f8baca3c02dbd2f4e45f1f529bd1f1c1
nicolelin0531 <nicolelin0531@yahoo.com.tw>
2022-03-13 17:45:24 +0800

    n tiers architecture

--- AbbyWeb/Pages/Admin/Categories/Index.cshtml.cs
+++ AbbyWeb/Pages/Admin/Categories/Index.cshtml.cs
@@ -4,7 +4,7 @@ using Abby.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AbbyWeb.Pages.Categories
+namespace AbbyWeb.Pages.Admin.Categories
 {
     public class IndexModel : PageModel
     {

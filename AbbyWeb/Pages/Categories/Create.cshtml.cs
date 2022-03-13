ed883e1c83b82f3843f5efb768b16d51012e238b
nicolelin0531 <nicolelin0531@yahoo.com.tw>
2022-03-13 10:28:40 +0800

    CategoryOnPost

---
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category{ get; set; }
+
+        public CreateModel(ApplicationDbContext db)    //DI
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}

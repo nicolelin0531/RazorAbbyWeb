92045b93e3fd13053a0e6910d721f42cc6cf726d
nicolelin0531 <nicolelin0531@yahoo.com.tw>
2022-03-13 11:33:23 +0800

    validation on client side

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -23,7 +23,7 @@ namespace AbbyWeb.Pages.Categories
         {
             if(Category.Name == Category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError(string.Empty, "The DisplayOrder cannot exactly match the Name,");
+                ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name,");
             }
             if (ModelState.IsValid)   //check post isValid (blank => not valid)
             {

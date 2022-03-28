97e1fbfde9fac70f06f9fd00179f2f4ed526430a
nicolelin0531 <nicolelin0531@yahoo.com.tw>
2022-03-28 11:21:19 +0800

    menuItem GET handler

--- AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -6,23 +6,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AbbyWeb.Pages.Admin.MenuItems;
-    [BindProperties]
-    public class UpsertModel : PageModel
+[BindProperties]
+public class UpsertModel : PageModel
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnvironment;
+    public MenuItem MenuItem { get; set; }
+    public IEnumerable<SelectListItem> CategoryList { get; set; }
+    public IEnumerable<SelectListItem> FoodTypeList { get; set; }
+
+    public UpsertModel(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)    //DI
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _hostEnvironment;
-        public MenuItem MenuItem { get; set; }
-        public IEnumerable<SelectListItem> CategoryList { get; set; }
-        public IEnumerable<SelectListItem> FoodTypeList { get; set; }
-    
-        public UpsertModel(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)    //DI
+        _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
+        MenuItem = new();
+    }
+    public void OnGet(int? id)
+    {
+        if (id != null)
         {
-            _unitOfWork = unitOfWork;
-            _hostEnvironment = hostEnvironment;
-            MenuItem = new();
+            //Edit
+            MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(x => x.Id == id);
         }
-        public void OnGet()
-        {
+
         CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem()
         {
             Text = i.Name,
@@ -37,22 +43,22 @@ namespace AbbyWeb.Pages.Admin.MenuItems;
 
     }
 
-        public async Task<IActionResult> OnPost()
-        {
-            string webRootPath = _hostEnvironment.WebRootPath;
-            var files = HttpContext.Request.Form.Files;
-        if(MenuItem.Id == 0)
+    public async Task<IActionResult> OnPost()
+    {
+        string webRootPath = _hostEnvironment.WebRootPath;
+        var files = HttpContext.Request.Form.Files;
+        if (MenuItem.Id == 0)
         {
             //create
             string filename_new = Guid.NewGuid().ToString();   //給名字 new filename, make sure all the file names are unique
             var uploads = Path.Combine(webRootPath, @"images\menuItems");  //路徑抓www/images底下圖 find out folder that are upload
             var extension = Path.GetExtension(files[0].FileName);  //make soure files have same extension
 
-            using(var fileStream = new FileStream(Path.Combine(uploads, filename_new + extension),FileMode.Create))
+            using (var fileStream = new FileStream(Path.Combine(uploads, filename_new + extension), FileMode.Create))
             {
                 files[0].CopyTo(fileStream);
             }
-            MenuItem.Image = @"\images\menuItems\" +filename_new + extension;
+            MenuItem.Image = @"\images\menuItems\" + filename_new + extension;
             _unitOfWork.MenuItem.Add(MenuItem);
             _unitOfWork.Save();
         }
@@ -60,7 +66,7 @@ namespace AbbyWeb.Pages.Admin.MenuItems;
         {
 
         }
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
+}
 

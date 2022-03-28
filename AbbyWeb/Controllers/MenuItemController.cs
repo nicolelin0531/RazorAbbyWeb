98f53242fef7ddcd0cd3a01b57d4dfda0c9a15aa
nicolelin0531 <nicolelin0531@yahoo.com.tw>
2022-03-28 13:22:09 +0800

    menuItem Delete Handler

--- AbbyWeb/Controllers/MenuItemController.cs
+++ AbbyWeb/Controllers/MenuItemController.cs
@@ -8,15 +8,32 @@ namespace AbbyWeb.Controllers
     public class MenuItemController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public MenuItemController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
         [HttpGet]
         public IActionResult Get()
         {
             var menuItemList = _unitOfWork.MenuItem.GetAll(includeProperties: "Category,FoodType");
             return Json(new {data = menuItemList});
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id);
+            //delete the old image
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.MenuItem.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful."});
         }
     }
 }

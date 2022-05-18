7153d24bc28be2155bc4affce24bf2b3c2fb7fb4
nicolelin0531 <77452533+nicolelin0531@users.noreply.github.com>
2022-05-18 22:48:28 +0800

    Add to Shopping Cart(新增數量至購物車)

--- AbbyWeb/Pages/Customer/Home/Details.cshtml.cs
+++ AbbyWeb/Pages/Customer/Home/Details.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace AbbyWeb.Pages.Customer.Home;
 [Authorize]
@@ -18,10 +19,25 @@ public class DetailsModel : PageModel
     public ShoppingCart ShoppingCart { get; set; }
     public void OnGet(int id)
     {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        
         ShoppingCart = new()
         {
-            MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,FoodType")
+            ApplicationUserId = claim.Value,
+            MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,FoodType"),
+            MenuItemId = id
         };
+    }
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.ShoppingCart.Add(ShoppingCart);
+            _unitOfWork.Save();
+            return RedirectToPage("Index");
+        }
+        return Page();
     }
 }
     
--- AbbyWeb/obj/Debug/net6.0/AbbyWeb.MvcApplicationPartsAssemblyInfo.cs
+++ AbbyWeb/obj/Debug/net6.0/AbbyWeb.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Abby.DataAccess")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Abby.Models")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Abby.Utility")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 

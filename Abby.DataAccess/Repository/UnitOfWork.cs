73af06e5e4d737f702d05713661acacbd796580c
nicolelin0531 <77452533+nicolelin0531@users.noreply.github.com>
2022-05-24 22:26:18 +0800

    add applicationUser to Summary and add ApplicationUser to repository

--- Abby.DataAccess/Repository/UnitOfWork.cs
+++ Abby.DataAccess/Repository/UnitOfWork.cs
@@ -20,6 +20,7 @@ namespace Abby.DataAccess.Repository
             ShoppingCart = new ShoppingCartRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
         public ICategoryRepository Category { get; private set; }
         public IFoodTypeRepository FoodType { get; private set; }
@@ -27,6 +28,7 @@ namespace Abby.DataAccess.Repository
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
 
         public void Dispose()
         {

813a00a9885bc6834cbbc11f9cdf374af773b2a7
nicolelin0531 <77452533+nicolelin0531@users.noreply.github.com>
2022-06-03 22:00:49 +0800

    Implement  OrderConfirmation page

--- AbbyWeb/Pages/Customer/Cart/OrderConfirmation.cshtml.cs
+++ AbbyWeb/Pages/Customer/Cart/OrderConfirmation.cshtml.cs
@@ -1,3 +1,5 @@
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,25 @@ namespace AbbyWeb.Pages.Customer.Cart
 {
     public class OrderConfirmationModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public int OrderId { get; set; }
+        public OrderConfirmationModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id);
+            if(orderHeader.SessionId != null)
+            {
+                //using stripe.check (台灣沒有stripe以下改用綠界)
+                //var service = new SessionService
+            }
+            List<ShoppingCart> shoppingCarts = 
+                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.UserId).ToList();
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+            OrderId = id;
         }
     }
 }
